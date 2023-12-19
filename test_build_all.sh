cf44a693ca3ade10858e0872e8e48625098c4840
matte1 <matt.epperson@k2space.com>
2023-12-19 11:55:36 -0700

    feat: Add support for specifying different cstdlib
    We'd like to be able to specify either glibc or musl. This adds an extra
    layer of heirarchy that allows a user to specify not only the
    architecture and buildroot version but also the cstdlib.

--- test_build_all.sh
+++ test_build_all.sh
@@ -2,15 +2,17 @@
 set -e
 set -o xtrace
 
-bazel build --verbose_failures --platforms=@bazel_bootlin//platforms:x86-64-linux-gnu-2021.11-5 //test:test_cpp
-bazel build --verbose_failures --platforms=@bazel_bootlin//platforms:x86-64-linux-gnu-2021.11-5 //test:test_c
-bazel build --verbose_failures --platforms=@bazel_bootlin//platforms:x86-64-linux-gnu-2022.08-1 //test:test_cpp
-bazel build --verbose_failures --platforms=@bazel_bootlin//platforms:x86-64-linux-gnu-2022.08-1 //test:test_c
-bazel build --verbose_failures --platforms=@bazel_bootlin//platforms:x86-64-core-i7-linux-gnu-2020.08-1 //test:test_cpp
-bazel build --verbose_failures --platforms=@bazel_bootlin//platforms:x86-64-core-i7-linux-gnu-2020.08-1 //test:test_c
-bazel build --verbose_failures --platforms=@bazel_bootlin//platforms:aarch64-linux-gnu-2021.11-1 //test:test_cpp
-bazel build --verbose_failures --platforms=@bazel_bootlin//platforms:aarch64-linux-gnu-2021.11-1 //test:test_c
-bazel build --verbose_failures --platforms=@bazel_bootlin//platforms:aarch64-linux-gnu-2020.08-1 //test:test_cpp
-bazel build --verbose_failures --platforms=@bazel_bootlin//platforms:aarch64-linux-gnu-2020.08-1 //test:test_c
-bazel build --verbose_failures --platforms=@bazel_bootlin//platforms:armv7-eabihf-linux-gnu-2020.08-1 //test:test_cpp
-bazel build --verbose_failures --platforms=@bazel_bootlin//platforms:armv7-eabihf-linux-gnu-2020.08-1 //test:test_c
+bazel build --verbose_failures --platforms=@bazel_bootlin//platforms:x86-64-linux-glibc-2021.11-5 //test:test_cpp
+bazel build --verbose_failures --platforms=@bazel_bootlin//platforms:x86-64-linux-glibc-2021.11-5 //test:test_c
+bazel build --verbose_failures --platforms=@bazel_bootlin//platforms:x86-64-linux-glibc-2022.08-1 //test:test_cpp
+bazel build --verbose_failures --platforms=@bazel_bootlin//platforms:x86-64-linux-glibc-2022.08-1 //test:test_c
+bazel build --verbose_failures --platforms=@bazel_bootlin//platforms:x86-64-core-i7-linux-glibc-2020.08-1 //test:test_cpp
+bazel build --verbose_failures --platforms=@bazel_bootlin//platforms:x86-64-core-i7-linux-glibc-2020.08-1 //test:test_c
+bazel build --verbose_failures --platforms=@bazel_bootlin//platforms:aarch64-linux-glibc-2021.11-1 //test:test_cpp
+bazel build --verbose_failures --platforms=@bazel_bootlin//platforms:aarch64-linux-glibc-2021.11-1 //test:test_c
+bazel build --verbose_failures --platforms=@bazel_bootlin//platforms:aarch64-linux-glibc-2020.08-1 //test:test_cpp
+bazel build --verbose_failures --platforms=@bazel_bootlin//platforms:aarch64-linux-glibc-2020.08-1 //test:test_c
+bazel build --verbose_failures --platforms=@bazel_bootlin//platforms:aarch64-linux-musl-2021.11-1 //test:test_cpp
+bazel build --verbose_failures --platforms=@bazel_bootlin//platforms:aarch64-linux-musl-2021.11-1 //test:test_c
+bazel build --verbose_failures --platforms=@bazel_bootlin//platforms:armv7-eabihf-linux-glibc-2020.08-1 //test:test_cpp
+bazel build --verbose_failures --platforms=@bazel_bootlin//platforms:armv7-eabihf-linux-glibc-2020.08-1 //test:test_c
